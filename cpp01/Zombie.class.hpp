12bd078be15a34356b78a9bffd3b02d5f4aceb7d
Ferran Fornes Palacin <ffornes-@student.42barcelona.com>
2024-03-08 10:59:15 +0100

    Created gitignore and base files for cpp01

---
+++ cpp01/Zombie.class.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.class.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffornes- <ffornes-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/08 08:41:36 by ffornes-          #+#    #+#             */
+/*   Updated: 2024/03/08 08:44:45 by ffornes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_CLASS_H
+# define ZOMBIE_CLASS_H
+
+class Zombie {
+	
+	private:
+		/* data */
+	public:
+		Zombie( void );
+		~Zombie( void );
+};
+
+Zombie.class::Zombie.class(/* args */) {
+
+}
+
+Zombie.class::~Zombie.class() {
+
+}
+
+
+#endif
