6557a19d6f24b65afb3d6c8f76a53910f804bf4b
ffornesp <ffornesp@gmail.com>
2024-07-02 17:04:05 +0200

    Added basic constructor destructor stuff..

--- cpp06/ex00/include/ScalarConverter.hpp
+++ cpp06/ex00/include/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 16:30:14 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/07/02 14:39:32 by ffornes-         ###   ########.fr       */
+/*   Updated: 2024/07/02 17:02:07 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,10 @@ class	ScalarConverter {
 	private :
 
 	public :
+		ScalarConverter( void );
+		~ScalarConverter( void );
+		ScalarConverter( const ScalarConverter& copy );
+		ScalarConverter&	operator=( const ScalarConverter& copy );
+
 		static void convert( std::string ) ;		
 };
--- cpp06/ex00/src/ScalarConverter.cpp
+++ cpp06/ex00/src/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 13:00:45 by herz              #+#    #+#             */
-/*   Updated: 2024/07/02 15:13:31 by ffornes-         ###   ########.fr       */
+/*   Updated: 2024/07/02 17:01:22 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,26 @@
 	Bool			-	true		||	false
 
 
-static void StringLiterals( std::string str ) {
-	std::cout << "StringLiterals called with string : " << str << std::endl;
+static void printOutput( std::string type, std::string value ) {
+	std::cout << type << " : " << value << std::endl;
 }
 
 */
+
+ScalarConverter::ScalarConverter( void ) {}
+
+ScalarConverter::~ScalarConverter( void ) {}
+
+ScalarConverter::ScalarConverter( const ScalarConverter& copy )
+{
+	( void )copy;
+}
+
+ScalarConverter&	ScalarConverter::operator=( const ScalarConverter& copy )
+{
+	( void )copy;
+	return ( *this );
+}
 
 static int	validateInput( std::string str ) {
 	unsigned int	charCount = 0;
@@ -47,11 +62,20 @@ static int	validateInput( std::string str ) {
 
 	if ( str.length() == 1 && !isdigit(str[0]) )
 		return INVALID;
-
+	
 	if ( str == "-inff" || str == "+inff" || str == "-inf" || str == "+inf" )
 		return STRING_LITERAL;
 	else if ( str == "nan" || str == "true" || str == "false" )
 		return STRING_LITERAL;
+
+	if ( *str.begin() == '\'' && ( str.length() == 3 || str.length() == 4 )) {
+		if ( *(--str.end()) != '\'' )
+			return INVALID;
+		else if ( str.length() == 4 && str[1] != '\\' )
+			return INVALID;
+		else
+			return CHAR_LITERAL;
+	}
 
 	for ( std::string::iterator it=str.begin(); it!=str.end(); ++it ) {
 		if ( !isalpha(*it) ) {
@@ -76,16 +100,29 @@ static int	validateInput( std::string str ) {
 	return NUMBER_LITERAL;
 }
 
-static void regularNumbers( long double l ) {
+static void stringLiteral( std::string str ) {
+	std::cout << "STRING_LITERAL found : " << str << std::endl;
+
+}
+
+static void charLiteral( std::string str ) {
+	std::cout << "CHAR_LITERAL found : " << str << std::endl;
+//	char	c;
+
+//	c = str;
+//	std::cout << "Char : " << c << std::endl;
+}
+
+static void numberLiteral( long double l ) {
 	char	c;
 	int		i;
 	float	f;
 	double	d;
 
-	d = l;
-	f = l;
-	i = l;
-	c = l;
+	d = static_cast< double >( l );
+	f = static_cast< float >( l );
+	i = static_cast< int >( l );
+	c = static_cast< char >( l );
 	if ( l >= 0 && l <= 255 ) {
 		if ( isprint(c) )
 			std::cout << "Char : " << "\'" << c << "\'" << std::endl;
@@ -124,17 +161,22 @@ void ScalarConverter::convert( std::string str ) {
 	std::cout << "Input received : " << str << std::endl;
 
 	input = validateInput(str);
-	if ( input == INVALID ) {
-		std::cerr << "Input is invalid, please run program with one C++ literal in it\'s most common form." << std::endl;
-		if ( str.length() == 1 )
-			std::cerr << "\tUsage: ./convert \\\'c\\\'" << std::endl;
-		exit( 1 );
+	switch ( input ) {
+		case INVALID : 
+			std::cerr << "Input is invalid, please run program with one C++ literal in it\'s most common form." << std::endl;
+			if ( str.length() == 1 )
+				std::cerr << "\tUsage: ./convert \\\'c\\\'" << std::endl;
+			exit( 1 );
+		case STRING_LITERAL :
+			stringLiteral(str);
+			break ;
+		case CHAR_LITERAL :
+			charLiteral(str);
+			break ;
+		case NUMBER_LITERAL :
+			ss << str;
+			ss >> l;
+			numberLiteral(l);
+			break ;
 	}
-	else if ( input == NUMBER_LITERAL ) {
-		ss << str;
-		ss >> l;
-		regularNumbers(l);
-	}
-	else
-		std::cout << "Input is a string literal or a character literal" << std::endl;
 }
