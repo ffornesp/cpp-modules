3884300e27f16d1327d7dbb1bab18629a26c8eac
ffornesp <ffornesp@gmail.com>
2024-10-15 17:40:07 +0200

    Added tests to show that the Intern can create forms and they work properly

--- cpp05/ex03/src/main.cpp
+++ cpp05/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ffornes- <ffornes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 14:01:14 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/06/13 16:05:26 by ffornes-         ###   ########.fr       */
+/*   Updated: 2024/10/15 17:36:30 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,38 @@
 #include <iostream>
 
 int	main( void ) {
-	AForm*	form;
 	Intern	i;
+	AForm*	form;
+	AForm*	presidentialForm;
+	AForm*	shrubberyForm;
+	AForm*	robotomyForm;
 
-	form = i.makeForm("Presidential pardon", "Bender");
+	form = i.makeForm( "TIG form", "Oktorok" );
 	if ( form ) {
 		delete form;
 	}
+	presidentialForm = i.makeForm( "Presidential pardon", "Obama" );
+	shrubberyForm =  i.makeForm( "Shrubbery creation", "file" );
+	robotomyForm = i.makeForm( "Robotomy request", "Bender" );
+
+	Bureaucrat	b0( "Pedrito", 1 );
+	b0.signForm( *presidentialForm );
+	b0.signForm( *shrubberyForm );
+	b0.signForm( *robotomyForm );
+	
+	std::cout << std::endl;
+	std::cout << *presidentialForm << std::endl;
+	std::cout << *shrubberyForm << std::endl;
+	std::cout << *robotomyForm << std::endl;
+
+	b0.executeForm( *presidentialForm );
+	b0.executeForm( *shrubberyForm );
+	b0.executeForm( *robotomyForm );
+
+	if ( presidentialForm )
+		delete presidentialForm;
+	if ( shrubberyForm )
+		delete shrubberyForm;
+	if ( robotomyForm )
+		delete robotomyForm;
 }
