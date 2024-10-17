0c20e22a85dd9dc6602409ce5a478f9738043332
ffornes- Bluebird <ffornesp@gmail.com>
2024-10-17 16:46:52 +0200

    Worked on array

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 17:41:10 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/08 18:51:18 by ffornes-         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:12:16 by herz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 template< class T >
 public Class Array {
-
+	private:
+		unsigned int	_size;
+		T				*_array;
+	public:
+		Array( void );
+		Array( unsigned int	n );
+		Array( const Array& old );
+		~Array( void );
+		Array&	operator=( const Array& old );
+		unsigned int	size( void ) const;
+		T&		operator[]( unsigned int n );
+		T&		operator[]( const unsigned int n ) const ;
 } ;
+
+#include "Array.tpp"
