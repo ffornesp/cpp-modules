5d21a81e6ba276d81fa2cba8137e39620979b118
ffornesp <ffornesp@gmail.com>
2024-10-17 18:10:56 +0200

    Finished project

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -6,14 +6,16 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 17:41:10 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/17 13:12:16 by herz             ###   ########.fr       */
+/*   Updated: 2024/10/17 18:05:19 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
+
 template< class T >
-public Class Array {
+class Array {
 	private:
 		unsigned int	_size;
 		T				*_array;
@@ -26,6 +28,73 @@ public Class Array {
 		unsigned int	size( void ) const;
 		T&		operator[]( unsigned int n );
 		T&		operator[]( const unsigned int n ) const ;
-} ;
+};
 
-#include "Array.tpp"
+template< class T >
+std::ostream &operator<<( std::ostream& os, const Array< T >& obj );
+
+template< typename T >
+Array< T >::Array( void ) {
+	this->_size = 0;
+	this->_array = NULL;
+}
+
+template< typename T >
+Array< T >::Array( unsigned int n ) {
+	this->_size = n;
+	this->_array = new T[ n ]();
+}
+
+template< typename T >
+Array< T >::Array( const Array< T >& old ) {
+	this->_size = old.size();
+	this->_array = new T[ this->_size ]();
+	if ( this->_size != 0 ) {
+		for ( unsigned int i = 0; i < this->_size ; i++ ) {
+			this->_array[i] = old._array[i];
+		}
+	}
+}
+
+template< typename T >
+Array< T >::~Array( void ) {
+	if ( this->_size )
+		delete[] this->_array;
+}
+
+template< typename T >
+Array< T >&	Array< T >::operator=( const Array< T >& old ) {
+	this->_size = old.size();
+	if ( this->_array )
+		delete[] this->_array;
+	if ( this->_size ) {
+		this->_array = new T[ this->_size ]();
+		for ( unsigned int i = 0; i < this->_size ; i++ ) {
+			this->_array[i] = old._array[i];
+		}
+	}
+	return *this;
+}
+
+template< class T >
+unsigned int Array< T >::size( void ) const {
+	return this->_size;
+}
+
+template< typename T >
+T&			Array< T >::operator[]( unsigned int n ) {
+	if ( !this->_array )
+		throw std::logic_error( "Trying to access an empty array\n" );
+	else if ( this->_size >= n )
+		throw std::out_of_range( "Index is out of range\n" );
+	return ( this->_array[ n ] );
+}
+
+template< typename T >
+T&	Array< T >::operator[]( unsigned int n ) const {
+	if ( !this->_array )
+		throw std::logic_error( "Trying to access an empty array\n" );
+	else if ( n >= this->_size )
+		throw std::out_of_range( "Index is out of range\n" );
+	return ( this->_array[ n ] );
+}
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -6,11 +6,83 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 17:40:45 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/08 17:40:56 by ffornes-         ###   ########.fr       */
+/*   Updated: 2024/10/17 18:10:41 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main( void  ) {
+#include "Array.hpp"
+#include <iostream>
 
-	return 0;
+int main() {
+    try {
+        // Test default constructor (empty array)
+        std::cout << "\033[32m" << "=== Test 1: Create Empty Array (Default Constructor) ===" << "\033[0m" << std::endl;
+        Array<int> arr1;
+        std::cout << "Size of arr1: " << arr1.size() << std::endl; // Should be 0
+        std::cout << "Is arr1 empty? " << (arr1.size() == 0 ? "Yes" : "No") << std::endl; // Should print Yes
+		// Trying to access the empty array
+		try {
+			for ( int i = 0 ; i < 5 ; i++ ) {
+				std::cout << arr1[i] << std::endl;
+			}
+		}
+		catch ( ...  ) {
+			std::cerr << "Index out of range" << std::endl;
+		}
+
+        // Test constructor with specific size
+        std::cout << "\033[32m" << "=== Test 2: Create Array with Specific Size ===" << "\033[0m" << std::endl;
+        Array<int> arr2(5);
+        std::cout << "Size of arr2: " << arr2.size() << std::endl; // Should be 5
+
+        // Display values
+        std::cout << "\033[32m" << "=== Test 3: Assign and Display Values (int array) ===" << "\033[0m" << std::endl;
+        // Assign values
+        for (unsigned int i = 0; i < arr2.size(); ++i) {
+            arr2[i] = i * 10;
+        }
+        for (unsigned int i = 0; i < arr2.size(); ++i) {
+            std::cout << "arr2[" << i << "] = " << arr2[i] << std::endl;
+        }
+
+        // Test with complex type (std::string)
+        std::cout << "\033[32m" << "=== Test 4: Create Array of std::string ===" << "\033[0m" << std::endl;
+        Array<std::string> arr3(3);
+
+        // Display std::string values
+        std::cout << "\033[32m" << "=== Test 5: Assign and Display Values (std::string array) ===" << "\033[0m" << std::endl;
+        arr3[0] = "Hello";
+        arr3[1] = "World";
+        arr3[2] = "C++";
+        for (unsigned int i = 0; i < arr3.size(); ++i) {
+            std::cout << "arr3[" << i << "] = " << arr3[i] << std::endl;
+        }
+
+        // Test copy constructor with std::string
+        std::cout << "\033[32m" << "=== Test 6: Copy Constructor (std::string array) ===" << "\033[0m" << std::endl;
+        Array<std::string> arr4(arr3);
+        std::cout << "Size of arr4 (copy of arr3): " << arr4.size() << std::endl;
+        std::cout << "arr4[0]: " << arr4[0] << std::endl;
+
+        // Test assignment operator with std::string
+        std::cout << "\033[32m" << "=== Test 7: Assignment Operator (std::string array) ===" << "\033[0m" << std::endl;
+        Array<std::string> arr5;
+        arr5 = arr3;
+        std::cout << "Size of arr5 (assignment from arr3): " << arr5.size() << std::endl;
+        std::cout << "arr5[1]: " << arr5[1] << std::endl;
+
+        // Test out of bounds access
+        std::cout << "\033[32m" << "=== Test 8: Out of Bounds Access ===" << "\033[0m" << std::endl;
+        try {
+            std::cout << arr2[5] << std::endl; // Should throw exception
+        } 
+		catch (const std::exception& e) {
+            std::cerr << "\033[31m" << "Exception caught: Out of bounds access!" << "\033[0m" << std::endl;
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "\033[31m" << "Exception caught: " << e.what() << "\033[0m" << std::endl;
+    }
+
+    return 0;
 }
