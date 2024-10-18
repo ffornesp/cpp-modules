e65e981b9aaa9c26fa0ce056c22314c2c5973b20
ffornes- Bluebird <ffornesp@gmail.com>
2024-10-18 19:10:05 +0200

    Implemented function to add range of iterators into span

--- cpp08/ex01/include/Span.hpp
+++ cpp08/ex01/include/Span.hpp
@@ -6,13 +6,14 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:49:48 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/18 17:15:14 by herz             ###   ########.fr       */
+/*   Updated: 2024/10/18 19:06:20 by herz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <set>
+#include <stdexcept>
 
 class Span {
 	private:
@@ -25,7 +26,18 @@ class Span {
 		Span( const Span& s );
 		Span&	operator=( const Span& copy );
 
-		void			addNumber( int n );
+		void	addNumber( int n );
+		template< typename T >
+		void	addNumbers( T& t );
 		unsigned long	shortestSpan( void );
 		unsigned long	longestSpan( void );
 };
+
+template< typename T >
+void	Span::addNumbers( T& t ) {
+	typename T::iterator it = t.begin();
+	typename T::iterator ite = t.end();
+	if ( std::distance( it, ite ) + this->_content.size() > this->_size )
+		throw std::runtime_error( "Content is unable to handle this amount of elements\n" );
+	this->_content.insert( it, ite );
+}
--- cpp08/ex01/src/main.cpp
+++ cpp08/ex01/src/main.cpp
@@ -6,26 +6,16 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:49:48 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/18 18:00:37 by herz             ###   ########.fr       */
+/*   Updated: 2024/10/18 19:09:37 by herz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 #include <iostream>
 #include <limits>
+#include <vector>
 
 int	main( void ) {
-	Span	s( 10000 );
-	
-	for ( int i = 0; i < 10000; i++ ) {
-		s.addNumber( i * 2 );
-	}
-	std::cout << std::endl << "\t\tREGULAR TESTS" << std::endl;
-	std::cout << std::endl;
-	std::cout << "Shortest span : " << s.shortestSpan() << std::endl;
-	std::cout << "Longest span : " << s.longestSpan() << std::endl;
-	std::cout << std::endl;
-
 // SUBJECT TEST
 	std::cout << "\tSubject test:" << std::endl;
 	Span	sp = Span(5);
@@ -37,7 +27,36 @@ int	main( void ) {
 	sp.addNumber(11);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+// MY TESTS
+// Regular add numbers
+	Span	s( 10000 );
+	
+	for ( int i = 0; i < 10000; i++ ) {
+		s.addNumber( i * 2 );
+	}
+	std::cout << std::endl << "\t\tREGULAR TESTS" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Shortest span : " << s.shortestSpan() << std::endl;
+	std::cout << "Longest span : " << s.longestSpan() << std::endl;
+	std::cout << std::endl;
+// Adding numbers iterator range
+	try {
+		std::vector< int >	v;
+		int					a[10] = { 90, 15, -22, 83, 44, -54, 61, 17, 89, 39 };
 
+		for ( int i = 0; i < 10; i++ )
+			v.push_back( a[ i ] );
+
+		Span				sv(10);
+
+		sv.addNumbers( v );
+		std::cout << "Shortest span : " << sv.shortestSpan() << std::endl;
+		std::cout << "Longest span : " << sv.longestSpan() << std::endl;
+		std::cout << std::endl;
+	}
+	catch ( std::exception& e ) {
+		std::cerr << e.what() << std::endl;
+	}
 // EXCEPTIONS
 	std::cout << std::endl << "\t\tEXCEPTION HANDLING" << std::endl;
 	std::cout << std::endl;
