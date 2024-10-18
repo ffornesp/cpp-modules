62268f661e853255cf6f8b5665a826d5be5cd2df
ffornes- Bluebird <ffornesp@gmail.com>
2024-10-18 16:05:36 +0200

    Better main

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:49:48 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/18 15:36:43 by herz             ###   ########.fr       */
+/*   Updated: 2024/10/18 16:05:17 by herz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,9 @@ int	main( void ) {
 	std::vector< int >	myVector;
 	std::deque< int >	myDeque;
 	std::list< int >	myList;
-
 	int					a[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	int					f;
-	
-	f = 0;
+	const int			f = 9;
+
 	for ( int i = 0; i < 10; i++ ) {
 		myVector.push_back( a[ i ] );
 		myDeque.push_back( a[ i ] );
