074c5f7d3adb357e51bf7f5fb56e9e18e92e6554
ffornes- Bluebird <ffornesp@gmail.com>
2024-10-18 15:37:47 +0200

    Finished ex00

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -6,10 +6,33 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:59:40 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/17 20:30:54 by herz             ###   ########.fr       */
+/*   Updated: 2024/10/18 15:36:54 by herz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
 
+template< typename T >
+int		easyFind( T& t, int n ) {
+	for ( typename T::iterator it = t.begin() ; it != t.end() ; ++it ) {
+		if ( *it == n )
+			return n;
+	}
+	throw std::runtime_error( "Unable to find " );
+}
+
+template< typename T >
+void	test( T& t, int n ) {
+	try {
+		easyFind< T >( t, n );
+		std::cout << "Found " << n << " in vector\n";
+	}
+	catch ( std::exception& e ) {
+		std::cerr << e.what() << n << std::endl;
+	}
+}
--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -6,13 +6,31 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:49:48 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/10/17 20:30:22 by herz             ###   ########.fr       */
+/*   Updated: 2024/10/18 15:36:43 by herz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <vector>
+#include <deque>
+#include <list>
 
 int	main( void ) {
+	std::vector< int >	myVector;
+	std::deque< int >	myDeque;
+	std::list< int >	myList;
 
+	int					a[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int					f;
+	
+	f = 0;
+	for ( int i = 0; i < 10; i++ ) {
+		myVector.push_back( a[ i ] );
+		myDeque.push_back( a[ i ] );
+		myList.push_back( a[ i ] );
+	}
+	test( myVector, f );	
+	test( myDeque, f );
+	test( myList, f );
 	return 0;
 }
