176571575cee4b05323310374c2e043206baba2e
ffornesp <ffornesp@gmail.com>
2024-10-21 11:48:20 +0200

    Added base files

---
+++ cpp09/ex00/src/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 11:34:28 by ffornes-          #+#    #+#             */
+/*   Updated: 2024/10/21 11:45:57 by ffornes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int	main( int argc, char *argv[] ) {
+	if ( argc > 2 )
+		std::cerr << "This program can only run with one file" << std::endl;
+	else {
+		
+	}
+	return 0;
+}
