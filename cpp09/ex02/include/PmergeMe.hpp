0aab3da2e7f63686a487d4700f17b82638c2bbed
Hz <ffornesp@gmail.com>
2024-11-15 09:18:32 +0100

    Moved comment here

--- cpp09/ex02/include/PmergeMe.hpp
+++ cpp09/ex02/include/PmergeMe.hpp
@@ -24,6 +24,8 @@
 # define DECREMENT	1
 #endif
 
+void	checkInput( char *argv[] );
+
 void	mergeInsertionSort( std::deque< int >& );
 
 template< typename T >
@@ -32,3 +34,48 @@ void	printContent( T& t ) {
 		std::cout << *it << " ";
 	std::cout << std::endl;
 }
+
+/*
+		WHAT TO DO?
+
+	call merge-insertion-sort
+		element_size = 1
+		size = 9
+
+	5 2 3 1 4 7 6 9 8 
+
+	swap content inside pairs
+
+	[5-2] [3-1] [4-7] [6-9] | 8				
+	[2-5] [1-3] [4-7] [6-9] | 8
+
+	call merge-insertion-sort
+		element_size = 2
+		size = 4
+
+	swap content inside pairs
+
+	[(2-5) (1-3)] [(4-7) (6-9)]
+	[(1-3)-(2-5)] [(4-7)-(6-9)]
+
+	call merge-insertion-sort
+		element_size = 4
+		size = 2
+
+	swap content inside pairs
+
+	[((1-3)-(2-5)) ((4-7)-(6-9))] 		since 5 < 9 we do not swap content
+	[((1-3)-(2-5))-((4-7)-(6-9))] 
+
+	call merge-insertion-sort
+		element_size = 8
+		size = 1 ; then return
+
+	
+	call jacobsthal haha xdxd
+
+	when calling jacobsthal we will keep the reference of the paired number using an iterator
+	the main difference between deque and list is that list will be able to keep it's iterator
+	intact event tho we insert stuff in between, however that's not the case for deque
+
+*/
