f3a96dd7e3e33f524baacab70547abcc253db989
Hz <ffornesp@gmail.com>
2024-11-15 09:17:33 +0100

    Moved functions into new file

---
+++ cpp09/ex02/src/parse.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 12:07:05 by ffornes-          #+#    #+#             */
+/*   Updated: 2024/11/14 19:25:29 by ffornes-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <climits>
+#include <cstring>
+
+static void badInput( std::string msg ) {
+	std::cout << "Error: invalid input" << msg << std::endl;
+	exit( 1 );
+}
+
+void	checkInput( char *argv[] ) {
+	long				l;
+	std::list< int >	myList;
+
+	for ( int i = 0; argv[i] != NULL; i++ ) {
+		l = atol( argv[i] );
+		if ( l < 0 )
+			badInput( ", there can't be any negative numbers." );
+		else if ( l > INT_MAX )
+			badInput( ", out of integer range." );
+		for ( int j = 0; argv[i][j] != '\0'; j++ )
+			if ( !isdigit( argv[i][j] ) )
+				badInput( ", non digit characters found." );
+		for ( std::list< int >::iterator it = myList.begin(); it != myList.end(); it++ )
+			if ( static_cast< int >( l ) == *it )
+				badInput( ", duplicates found." );
+		myList.push_back( static_cast< int >( l ) );
+	}
+}
