a83c38b21be46e780844dd716f9546034978f8a4
ffornesp <ffornesp@gmail.com>
2024-11-21 16:38:13 +0100

    Deleted script

--- cpp09/ex02/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create an array of numbers from 1 to 21
-numbers=( $(seq 1 21) )
-
-# Shuffle and output the numbers
-./PmergeMe $(echo "${numbers[@]}" | tr ' ' '\n' | shuf | tr '\n' ' ')
