690279f5710b82dd067f6ea1e99666544032fb19
ffornesp <ffornesp@gmail.com>
2024-11-25 19:19:02 +0100

    Implemented checker function

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -6,12 +6,39 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 12:07:05 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/11/21 16:36:30 by ffornes-         ###   ########.fr       */
+/*   Updated: 2024/11/25 19:18:45 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 #include <ctime>
+#include <algorithm>
+
+static bool	isSorted( std::deque< int > myDeque, std::deque< int > & ogDeque )
+{
+	std::sort(ogDeque.begin(), ogDeque.end());
+	std::deque< int >::iterator ogIt = ogDeque.begin();
+	for (std::deque< int >::iterator it = myDeque.begin(); it != myDeque.end(); ++it)
+	{
+		if (*it != *ogIt)
+			return false;
+		++ogIt;
+	}
+	return true;
+}
+
+static bool	isSorted( std::list< int > & myDeque, std::list< int > & ogDeque )
+{
+	ogDeque.sort();
+	std::list< int >::iterator ogIt = ogDeque.begin();
+	for (std::list< int >::iterator it = myDeque.begin(); it != myDeque.end(); ++it)
+	{
+		if (*it != *ogIt)
+			return false;
+		++ogIt;
+	}
+	return true;
+}
 
 int	main( int argc, char *argv[] ) {
 	if ( argc < 3 ) {
@@ -28,6 +55,9 @@ int	main( int argc, char *argv[] ) {
 		myList.push_back( n );
 	}
 
+	std::deque< int >	ogDeque( myDeque );
+	std::list< int >	ogList( myList );
+	
 	clock_t	start;
 	clock_t	end;
 
@@ -50,6 +80,13 @@ int	main( int argc, char *argv[] ) {
 	printContent( myDeque );
 	std::cout << "Time to process a range of " << argc - 1 << " elements with std::[deque] : " << dequeTime << " us" << std::endl;
 	std::cout << "Time to process a range of " << argc - 1 << " elements with std::[list] : " << listTime << " us" << std::endl;
-
+	if ( !isSorted( myDeque, ogDeque ) )
+		std::cout << YELLOW << "DEQUE NOT SORTED" << std::endl;
+	else
+		std::cout << "DEQUE SORTED" << std::endl;
+	if	( !isSorted( myList, ogList ) )
+		std::cout << YELLOW << "LIST NOT SORTED" << std::endl;
+	else
+		std::cout << "LIST SORTED" << std::endl;
 	return 0;
 }
