236d2bf7bb3d24e9de6f529800994891d26f541f
ffornesp <ffornesp@gmail.com>
2024-11-28 19:30:23 +0100

    Included calculate

--- cpp09/ex01/include/RPN.hpp
+++ cpp09/ex01/include/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: ffornes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 16:29:41 by ffornes-          #+#    #+#             */
-/*   Updated: 2024/11/21 18:51:26 by ffornes-         ###   ########.fr       */
+/*   Updated: 2024/11/28 19:00:14 by ffornes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 #include <string>
 #include <iostream>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <cfloat>
 
 std::string	removeSpaces( const std::string& );
 bool		validateInput( const std::string str );
 
 void	RPN( const std::string );
+bool	calculate( long double& n1, long double n2, char operation );
